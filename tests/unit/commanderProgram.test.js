cb42b278ee77afa7711fba79ada7cdc9f45f1955
peterdanwan <peterdanwan@gmail.com>
2024-09-09 22:40:13 -0400

    fixed test to compare program.description() with colourizedDescription

--- tests/unit/commanderProgram.test.js
+++ tests/unit/commanderProgram.test.js
@@ -9,6 +9,11 @@ describe('commanderProgram.js tests', () => {
   test("The program's name, description, and version match what's in package.json", () => {
     expect(program.name()).toBe(name);
     expect(program.version()).toBe(version);
-    expect(program.description()).toBe(description);
+
+    const colourizedDescription = description.replace(
+      'gimme_readme',
+      '\x1b[34mgimme_readme\x1b[0m'
+    );
+    expect(program.description()).toBe(colourizedDescription);
   });
 });
